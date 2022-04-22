62afd3831a76984e89735e53ba29febd91f29391
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-04-22 13:11:59 -0500

    update for POD cleanup

--- lib/Suricata/Monitoring.pm
+++ lib/Suricata/Monitoring.pm
@@ -16,11 +16,11 @@ Suricata::Monitoring - LibreNMS JSON SNMP extend and Nagios style check for Suri
 
 =head1 VERSION
 
-Version 0.1.0
+Version 0.1.1
 
 =cut
 
-our $VERSION = '0.1.0';
+our $VERSION = '0.1.1';
 
 =head1 SYNOPSIS
 
